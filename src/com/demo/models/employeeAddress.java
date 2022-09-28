853d4885ae7eed9e2e90ed59617e946bd69d1e52
Kandukuri <AKANDUKU@ford.com>
2022-09-28 16:38:02 +0530

    update address model by ahalyareddy

---
+++ src/com/demo/models/employeeAddress.java
@@ -0,0 +1,26 @@
+package com.demo.models;
+
+public class employeeAddress {
+	
+	private String employeeAddress;
+
+		public employeeAddress() {
+			super();
+		// TODO Auto-generated constructor stub
+		}
+		public void setemployeeAddress(String employeeAddress) {
+			this.employeeAddress = employeeAddress;
+		}
+		public String getEmployeeAddress() {
+			return employeeAddress;
+		}
+	
+}
+		 
+		 
+		
+	
+	
+	
+
+}
